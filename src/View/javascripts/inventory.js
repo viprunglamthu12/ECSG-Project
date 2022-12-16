0eb92ce18d2558646f75db78df8c52fd353be3ca
viprunglamthu12 <phamduongtruongduc@gmail.com>
2022-12-16 21:01:03 +0700

    Update View

---
+++ src/View/javascripts/inventory.js
@@ -0,0 +1,15 @@
+function controlPriceToReceive() {
+    const yourPrice = document.getElementById("yourPrice");
+    yourPrice.value = yourPrice.value;
+
+    const youReceive = document.getElementById("youReceive");
+    youReceive.value = yourPrice.value * 0.98;
+
+}
+function controlReceiveToPrice() {
+    const youReceive = document.getElementById("youReceive");
+    youReceive.value = youReceive.value;
+    const yourPrice = document.getElementById("yourPrice");
+    yourPrice.value = youReceive.value * 1.02;
+
+}
